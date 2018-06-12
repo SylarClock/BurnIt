18b550a609725d153b2b794d35eafb11eea84b9c
Emmanuel Terrazas <emmanuel_sonicsw@hotmail.com>
2018-06-12 02:18:27 -0500

    Borrar por parte del administrador
    Solo si eres admin puedes borrar criticas, agregado controlador Admin, validacion en el HomeController para saber que tipo de usuario es, blade para el administrador aceptaRechaza y ruta del controlador Admin en web.php

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Admin extends Controller
+{
+    //
+    public function actionAdmin(Request $request)
+    {
+        $id = $request['id'];
+        
+        // $aceptar = $request->input('aceptar');
+        $rechazar = $request->input('borrar');
+
+        // if(isset($aceptar))
+        // {
+        //     $review = DB::table('posts')->where('id', $id)->get();
+        //     $review->activo = true; 
+        //     //$review->save();
+        // }
+        if(isset($rechazar))
+        {
+            //DB::table('posts')->where('id', $id)->update(['active' => false]);
+            DB::table('posts')->where('id', $id)->delete();
+        }
+
+        return redirect('/');
+    }
+}
